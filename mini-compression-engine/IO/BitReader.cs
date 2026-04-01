fbf0d84ec2333b9b6dac58a94eda65a6651f333a
uditnm <udit20001@gmail.com>
2026-04-02 02:38:36 +0530

    Changes: Implement basic bit reader and writer

---
+++ mini-compression-engine/IO/BitReader.cs
@@ -0,0 +1,41 @@
+﻿namespace mini_compression_engine.IO;
+
+public class BitReader
+{
+    public BitReader(byte[] byteData, int bitCount)
+    {
+        ByteData = byteData;
+        BitCount = bitCount;
+    }
+
+    private byte[] ByteData { get; set; }
+    private int BitCount { get; set; }
+    private int CurrentBitIndex = 8;
+    private int CurrentByteIndex = 0;
+    private int BitsRead = 0;
+
+    public bool Read()
+    {
+        if (CurrentBitIndex == 0)
+        {
+            CurrentBitIndex = 8;
+            CurrentByteIndex++;
+        }
+
+        if (CurrentByteIndex >= ByteData.Length)
+        {
+            throw new InvalidOperationException("No more data to read.");
+        }
+        var data = ByteData[CurrentByteIndex];
+
+        if (BitsRead < BitCount)
+        {
+            var bit = (data >> (CurrentBitIndex - 1)) & 1;
+            CurrentBitIndex--;
+            BitsRead++;
+            return Convert.ToBoolean(bit);
+        }
+
+        throw new InvalidOperationException("No more data to read.");
+    }
+}
---
+++ mini-compression-engine/IO/BitWriter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mini_compression_engine.IO;
+
+public class BitWriter
+{
+    public byte CurrentByte = 0;
+    public List<byte> ByteData { get; set; } = new();
+    int BitCount = 0;
+    public void Write(bool bitToUse)
+    {
+        byte bit = Convert.ToByte(bitToUse);
+        CurrentByte = (byte)(CurrentByte << 1);
+        CurrentByte = (byte)(CurrentByte | bit);
+        BitCount++;
+
+        if (BitCount == 8)
+        {
+            ByteData.Add(CurrentByte);
+            BitCount = 0;
+            CurrentByte = 0;
+        }
+    }
+
+    public void Flush()
+    {
+        if (BitCount == 0)
+        {
+            return;
+        }
+
+        CurrentByte = (byte)(CurrentByte << (8 - BitCount));
+        ByteData.Add(CurrentByte);
+
+        BitCount = 0;
+        CurrentByte = 0;
+    }
+}
